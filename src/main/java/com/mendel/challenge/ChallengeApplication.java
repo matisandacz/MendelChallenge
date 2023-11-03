6ab8b00f8a7096e08368bf08db5685b5f96bac32
Matias <modandyxx@hotmail.com>
2023-11-03 13:26:35 -0300

    Initializing the Repository

---
+++ src/main/java/com/mendel/challenge/ChallengeApplication.java
@@ -0,0 +1,16 @@
+/*
+ *(C) 2023
+ *Author: Matias Sandacz
+ */
+package com.mendel.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChallengeApplication.class, args);
+  }
+}
---
+++ src/test/java/com/mendel/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+ *(C) 2023
+ *Author: Matias Sandacz
+ */
+package com.mendel.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
